a692e755eec48cd1df890e86662d93ddb079b057
bsprink <bsprink@comcast.net>
2017-04-17 19:40:55 -0500

    functioning

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
-alert();
-7KQEYiAF5zeVxe4ozOXjBieqCtwMPrBpUAAKwUER
+//alert();
+
+var app = angular.module('spaceApp', []);
+app.controller('spaceCtrl', function($scope, $http) {
+    $scope.func = function(){
+    	alert();
+console.log("https://api.nasa.gov/EPIC/api/natural/date/" + $("#dateInputArea").val() + "?api_key=7KQEYiAF5zeVxe4ozOXjBieqCtwMPrBpUAAKwUER")
+    	$http.get("https://api.nasa.gov/EPIC/api/natural/date/" + $("#dateInputArea").val() + "?api_key=7KQEYiAF5zeVxe4ozOXjBieqCtwMPrBpUAAKwUER")
+    	.then(function(response){
+    		$scope.space = response.data;
+            console.log($scope.space);
+
+    	});
+    };
+});
